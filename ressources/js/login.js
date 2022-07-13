840dde6a767e1c32bd8bf567a8e8733d180c37fe
Romain Leneveu <leneveu.romain@gmail.com>
2022-07-13 23:52:08 +0200

    other try

--- ressources/js/login.js
+++ ressources/js/login.js
@@ -309,7 +309,7 @@ class ClassLogin {
     static openWS() {
         login.currentPassword.input[0].blur();
         socket?.close();
-        let host = "ws://" + window.location.host;
+        let host = "wss://" + window.location.host;
         socket = new WSConnection(host);
     }
     phoneCheck() {
